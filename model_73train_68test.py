d38fc8d0a75cbd2885473e1d7dd38c250b9c4db4
Bartosz Ambroż <151546547+BiBi4K@users.noreply.github.com>
2025-05-09 17:53:33 +0200

    Update model_73train_68test.py

--- model_73train_68test.py
+++ model_73train_68test.py
@@ -75,8 +75,8 @@ transform_train = transforms.Compose([
 ])
 
 transform_test = transforms.Compose([
-    transforms.Resize(40),
-    transforms.CenterCrop(40),
+    transforms.Resize(48),
+    transforms.CenterCrop(48),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
